be33bc4a81a38967db188a22087490e4dc67fbbf
Tenghuy <tenghuyly35@gmailcom>
2024-12-23 13:16:22 +0700

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-undef */
-const flowbite = require("flowbite-react/tailwind");
+// import flowbite from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    flowbite.plugin(),
+    require('flowbite/plugin')
   ],
 };
